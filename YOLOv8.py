bf6e28adad2a4c130f50c4ff5affc7598976cae6
ryan-beaudoin <147445985+ryan-beaudoin@users.noreply.github.com>
2024-08-13 23:28:11 -0500

    Delete YOLOv8.py

--- YOLOv8.py
+++
@@ -1,5 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO("your_model_name.pt")
-
-model.predict(source = "chosen_competition_video.mp4", show=True, save=True, hide_labels=True, conf=0.5, save_txt=False, save_crop=False, line_thickness=2)
