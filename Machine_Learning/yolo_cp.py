b5a1a7e1ecff9b2b71b8be8cb1ce7b12c21fb8dd
ryw27 <rywbook06@gmail.com>
2024-10-04 13:46:43 -0400

    yolo_cp

--- Machine_Learning/yolo_cp.py
+++ Machine_Learning/yolo_cp.py
@@ -3,19 +3,20 @@ from ultralytics import YOLO
 import numpy as np
 
 def video_inference():
-    model = YOLO('path_to_model.pt')
+    model = YOLO('best.pt')
 
     #TODO grab a video feed using cv2 to define cap. Use a video from the UMARV CV dropbox
-    cap = None
+    video_path = "comp24_colored_cones_1.mp4"
+    cap = cv2.VideoCapture(video_path) 
 
     #TODO Get image height and width and assign to 2 variables
     #Look under video capture properties section -> https://docs.opencv.org/3.4/d4/d15/group__videoio__flags__base.html
     #Use Hint #1 if you struggle to implement this
-    image_width = None
-    image_height = None
+    image_width = cv2.CAP_PROP_FRAME_WIDTH 
+    image_height = cv2.CAP_PROP_FRAME_HEIGHT 
 
     #TODO Setup occupancy grid - create a numpy array of zeros with defined image width and height
-    occupancy_grid = None
+    occupancy_grid = np.zeros((image_width,image_height),dtype=np.int32)
 
     while (cap.isOpened()):
       
@@ -23,29 +24,35 @@ def video_inference():
         if success: 
 
             #TODO refer to YOLO docs to define results as a model -> (near bottom) https://docs.ultralytics.com/modes/predict/#streaming-source-for-loop 
-            results = None
+            results = model(frame) 
 
             if results[0].masks is not None:
                     if(len(results[0].masks.xy) != 0):
                         
                         annotated_frame = results[0].plot()    
                         segment = results[0].masks.xy[0] 
-
                         #TODO Transfer segment into a numpy array 
                         #*you will have to pass in a second argument for the datatype
                         #to ensure it is compatible for cv2, dtype=np.int32*
-                        segment_array = None
+                        segment_array = np.array([segment],dtype=np.int32) 
 
                         #TODO Find cv2 function to create segmenation maskes, take note of required parameters
                         #Use Hint #2 if you cannot find the function
-                       
+                        
+                        segmasks = cv2.fillPoly(occupancy_grid,segment_array,[255,0,0])
                         
                         #TODO use cv2.imshow to display your occupancy grid and annotated frames (separately)
-                        
-                       
+                        occupancy_grid = occupancy_grid.astype(np.uint8)
+                        cv2.imshow("occupancy_grid",occupancy_grid)
+                        cv2.waitKey(1)
+                        cv2.imshow("annotated_frame",annotated_frame)
                        
                         if cv2.waitKey(1) & 0xFF == ord("q"):
                             break
+        else:
+            break
 
 video_inference()
 
+cap.release()
+cv2.destroyAllWindows()
